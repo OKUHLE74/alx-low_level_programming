27177068f4240221d4c1e7a9bb71b7d4c30ae9c0
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-06-18 09:19:28 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - a program that prints
  * Return: 0 (success)
  */
-{
 int main(void)
-	printf(" with proper grammar, but the outcome is a piece of art,\n")
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 	return (0);
 }
