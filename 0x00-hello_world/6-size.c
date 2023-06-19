17817856bc2e434ba78d5d542df03984dbb0ce8c
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-06-19 11:13:25 -0700

    m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,9 @@
  * Return 0 (Success)
  */
 int main(void)
-{	char a;
+
+{
+	char a;
 	int b;
 	long int c;
 	long long int d;
@@ -14,5 +16,4 @@ printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
 }
