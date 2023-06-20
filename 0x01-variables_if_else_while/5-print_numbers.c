8ca66547789573b7440042d99c416750771d533b
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-06-20 11:17:14 -0700

    n

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,14 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void)		
+int main(void)	
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	printf("%d", i);
+
 	printf("\n");
+
 	return (0);
 }
