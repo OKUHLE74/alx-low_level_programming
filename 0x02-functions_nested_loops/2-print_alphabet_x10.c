98dd077b0c69acc0a4804265d71312c9fc08451e
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-06-22 07:16:11 -0700

    m

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char  c;
+	char c;
 	int i = 0;
 
 	while (i <= 0)
 	{
 	for (c = 'a'; c <= 'z'; c++)
 	{
-	_putchar(c);
-
+	_putchar(c);	
 	_putchar('\n');
 	i++;
+	}
 	}
 }
