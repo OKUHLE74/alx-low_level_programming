f6b968166605ca6802a14705124e6239b8cea8ad
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-06-25 06:49:49 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the first 98 Fibonacci numbers, starting with
- * 1 and 2, separated by comma followed by a space
+ * 1 and 2, separated by comma followed by a space.
  * Return: Always 0.
  */
 
@@ -18,13 +18,13 @@ int main(void)
 	sum = fib1 + fib2;
 	printf("%lu, ", sum);
 	fib1 = fib2;
-	fib2 = ssum;
+	fib2 = sum;
 	}
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
 	fib2_half2 = fib2 % 10000000000;
-	for (count = 93; count < 99;count++)
+	for (count = 93; count < 99; count++)
 	{
 	half1 = fib1_half1 + fib2_half1;
 	half2 = fib1_half2 + fib2_half2;
