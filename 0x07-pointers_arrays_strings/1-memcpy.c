fb19f7c4d44714b54633f9ab74bd97aeea301900
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-07-04 15:26:09 -0700

    1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 
 /**
  *_memcpy - Entry point
- * @dest: input 
+ * @dest: input
  * @src: input
  * @n: input
  * Return: Always 0 (success)
@@ -12,6 +12,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		dest[i] = src [i];
+	dest[i] = src[i];
 	return (dest);
 }
