862e6ea0dc531d74167d92fdc0fbfde87f31562c
OKUHLE74 <lindokuhlebetty@gmail.com>
2023-07-10 08:08:40 -0700

    lindo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc - agurment count
+ * @argv - argurment vector
+ *
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return 0;
+}
